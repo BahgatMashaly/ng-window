8242d82a25e0f11dd6425caf286f2e978e2c1e7e
mashaly100200 <bahgat@mashaly.com>
2017-03-18 10:27:39 +0200

    npm remove google text

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-/**
- * @module
- * @description
- * Entry point for all public APIs of the common package.
- */
+
 // export * from './src';
 export { BootstrapWindowContainer,DragEvent,IDragListener } from './src/BootstrapWindowContainer';
 export { WindowBackdrop } from './src/ModalBackdrop';
@@ -18,4 +7,4 @@ export {WindowConfig } from './src/WindowConfig';
 export {WindowInstance } from './src/WindowInstance';
 export {WindowsInjetor } from './src/WindowsInjetor';
 
-//# sourceMappingURL=index.js.map
+ 
